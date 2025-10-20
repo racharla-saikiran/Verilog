d17f4c95efa9ff15fbe21065893803be0235ad75
racharla-saikiran <77745382+racharla-saikiran@users.noreply.github.com>
2025-10-20 13:47:32 +0530

    Add files via upload

---
+++ Full Adder using half adder/tb.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module tb();
+    reg a,b,c; 
+    wire sum,carry;
+    integer i=0;
+    top DUT(a,b,c,sum,carry);
+ 
+   initial begin 
+       for(i=0;i<8;i=i+1) begin 
+            {a,b,c} = i;#10;
+       end 
+      $finish;
+   end
+endmodule
